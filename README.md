0eb411a177483465320b1103b909c696294117e1
Stefan Benicke <stefan.benicke@gmail.com>
2014-09-26 17:20:11 +0200

    version 1.0

---
+++ README.md
@@ -0,0 +1,56 @@
+uuid.js
+=======
+
+UUID version 4 generator and parser - string and binary
+
+[Wikipedia UUID](http://en.wikipedia.org/wiki/Universally_unique_identifier#Version_4_.28random.29)
+
+* runs in browsers and node.js 
+* creates and parses uuids as string or binary
+
+#Usage
+
+Return a valid v4 UUID.
+
+```javascript
+uuid()
+// e.g "6ee65cf0-4fdc-4d33-b78d-d09e1294d80f"
+```
+
+##Methods
+
+###`create`
+
+Same as uuid().
+
+```javascript
+var id = uuid.create();
+```
+
+###`createBinary`
+
+Return a v4 UUID as Uint8Array, Buffer or Array.
+
+```javascript
+var bytes = uuid.createBinary();
+// e.g. [110, 230, 92, 240, 79, 220, 77, 51, 183, 141, 208, 158, 18, 148, 216, 15]
+```
+
+###`parse`
+
+Parses a v4 UUID from string to binary
+
+```javascript
+var bytes = uuid.parse('6ee65cf0-4fdc-4d33-b78d-d09e1294d80f');
+// gives [110, 230, 92, 240, 79, 220, 77, 51, 183, 141, 208, 158, 18, 148, 216, 15]
+```
+
+###`parseBinary`
+
+Parses a v4 UUID from binary to string
+
+```javascript
+var bytes = new Uint8Array([110, 230, 92, 240, 79, 220, 77, 51, 183, 141, 208, 158, 18, 148, 216, 15]);
+var id = uuid.parseBinary(bytes);
+// gives "6ee65cf0-4fdc-4d33-b78d-d09e1294d80f"
+```
---
+++ uuid.js
@@ -0,0 +1,97 @@
+/*!
+ * UUID v4 generator and parser
+ * author: Stefan Benicke <stefan.benicke@gmail.com>
+ * version: 1.0
+ * license: MIT
+ */
+
+;(function(global) {
+	'use strict';
+
+	var getRandomBytes;
+	var BufferView = global.Buffer || global.Uint8Array || Array;
+
+	if ( typeof global.require === 'function') {
+		getRandomBytes = function() {
+			return global.require('crypto').randomBytes(16);
+		};
+	} else if (global.crypto && crypto.getRandomValues) {
+		getRandomBytes = function() {
+			var bytes = new Uint8Array(16);
+			return crypto.getRandomValues(bytes);
+		};
+	} else {
+		getRandomBytes = function() {
+			var bytes = [];
+			for (var i = 0; i < 16; i++) {
+				bytes[i] = Math.random() * 16 | 0;
+			}
+			return bytes;
+		};
+	}
+
+	function toHex(number) {
+		var hex = Number(number).toString(16);
+		if (hex.length & 1) {
+			return '0' + hex;
+		}
+		return hex;
+	}
+
+	function toNumber(hex) {
+		return parseInt(hex, 16);
+	}
+
+	function uuid() {
+		return uuid.create();
+	}
+
+
+	uuid.create = function() {
+		var bytes = uuid.createBinary();
+		return uuid.parseBinary(bytes);
+	};
+
+	uuid.createBinary = function() {
+		// version 4
+		var bytes = getRandomBytes();
+		bytes[6] = bytes[6] & 0x0f | 0x40;
+		bytes[8] = bytes[8] & 0x3f | 0x80;
+		return bytes;
+	};
+
+	uuid.parseBinary = function(bytes) {
+		var result = '';
+		for (var i = 0; i < 16; i++) {
+			if (i === 4 || i === 6 || i === 8 || i === 10) {
+				result += '-';
+			}
+			result += toHex(bytes[i]);
+		}
+		return result;
+	};
+
+	uuid.parse = function(string) {
+		var result = new BufferView(16);
+		var charCount = 0;
+		for (var i = 0; i < 16; i++) {
+			if (i === 4 || i === 6 || i === 8 || i === 10) {
+				charCount++;
+			}
+			result[i] = toNumber(string[charCount] + string[charCount + 1]);
+			charCount += 2;
+		}
+		return result;
+	};
+
+	if ( typeof define === 'function' && define.amd) {
+		define(function() {
+			return uuid;
+		});
+	} else if ( typeof module !== 'undefined' && module.exports) {
+		module.exports = uuid;
+	} else {
+		global.uuid = uuid;
+	}
+
+})(this);
