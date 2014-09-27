76c2682cabd9ea5e31f97912b76ad77b0e5d4c78
Stefan Benicke <stefan.benicke@gmail.com>
2014-09-27 10:37:47 +0200

    polished source code

--- uuid.js
+++ uuid.js
@@ -1,46 +1,12 @@
 /*!
  * UUID v4 generator and parser
  * author: Stefan Benicke <stefan.benicke@gmail.com>
- * version: 1.0
+ * version: 1.1
  * license: MIT
  */
 
 ;(function(global) {
 	'use strict';
-
-	var getRandomBytes;
-	var BufferView = global.Buffer || global.Uint8Array || Array;
-
-	if ( typeof global.require === 'function') {
-		getRandomBytes = function() {
-			return global.require('crypto').randomBytes(16);
-		};
-	} else if (global.crypto && crypto.getRandomValues) {
-		getRandomBytes = function() {
-			var bytes = new Uint8Array(16);
-			return crypto.getRandomValues(bytes);
-		};
-	} else {
-		getRandomBytes = function() {
-			var bytes = [];
-			for (var i = 0; i < 16; i++) {
-				bytes[i] = Math.random() * 16 | 0;
-			}
-			return bytes;
-		};
-	}
-
-	function toHex(number) {
-		var hex = Number(number).toString(16);
-		if (hex.length & 1) {
-			return '0' + hex;
-		}
-		return hex;
-	}
-
-	function toNumber(hex) {
-		return parseInt(hex, 16);
-	}
 
 	function uuid() {
 		return uuid.create();
@@ -54,7 +20,7 @@
 
 	uuid.createBinary = function() {
 		// version 4
-		var bytes = getRandomBytes();
+		var bytes = _getRandomBytes();
 		bytes[6] = bytes[6] & 0x0f | 0x40;
 		bytes[8] = bytes[8] & 0x3f | 0x80;
 		return bytes;
@@ -66,7 +32,7 @@
 			if (i === 4 || i === 6 || i === 8 || i === 10) {
 				result += '-';
 			}
-			result += toHex(bytes[i]);
+			result += _toHex(bytes[i]);
 		}
 		return result;
 	};
@@ -78,11 +44,45 @@
 			if (i === 4 || i === 6 || i === 8 || i === 10) {
 				charCount++;
 			}
-			result[i] = toNumber(string[charCount] + string[charCount + 1]);
+			result[i] = _toNumber(string[charCount] + string[charCount + 1]);
 			charCount += 2;
 		}
 		return result;
 	};
+
+	var BufferView = global.Buffer || global.Uint8Array || Array;
+
+	var _getRandomBytes;
+	if ( typeof global.require === 'function') {
+		_getRandomBytes = function() {
+			return global.require('crypto').randomBytes(16);
+		};
+	} else if (global.crypto && crypto.getRandomValues) {
+		_getRandomBytes = function() {
+			var bytes = new Uint8Array(16);
+			return crypto.getRandomValues(bytes);
+		};
+	} else {
+		_getRandomBytes = function() {
+			var bytes = [];
+			for (var i = 0; i < 16; i++) {
+				bytes[i] = Math.random() * 16 | 0;
+			}
+			return bytes;
+		};
+	}
+
+	function _toHex(number) {
+		var hex = Number(number).toString(16);
+		if (hex.length & 1) {
+			return '0' + hex;
+		}
+		return hex;
+	}
+
+	function _toNumber(hex) {
+		return parseInt(hex, 16);
+	}
 
 	if ( typeof define === 'function' && define.amd) {
 		define(function() {
